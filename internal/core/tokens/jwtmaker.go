86e8191b93cd8d04628fe1f8be26591c750837b6
Alexey Ponomarev <al.ponomarev@astondevs.ru>
2023-06-04 16:55:12 +0300

    more jwt logic

--- internal/core/tokens/jwtmaker.go
+++ internal/core/tokens/jwtmaker.go
@@ -1,6 +1,7 @@
 package tokens
 
 import (
+	"fmt"
 	"log"
 	"time"
 
@@ -35,4 +36,66 @@ func (j *JWTMaker) CreateToken(userID uuid.UUID, rTokenID uuid.UUID, lifetime ti
 	}
 
 	return tokenString, payload.RtokenID, nil
+}
+
+func (j *JWTMaker) ParseToken(token string) (*TokenPayload, error) {
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, fmt.Errorf("wrong signing method")
+		}
+
+		return []byte(j.secretKey), nil
+	}
+	parsedToken, err := jwt.ParseWithClaims(token, &TokenPayload{}, keyFunc)
+	if err != nil {
+		verr, _ := err.(*jwt.ValidationError) //nolint: errorlint
+		if verr.Errors == jwt.ValidationErrorClaimsInvalid {
+			_, ok := parsedToken.Claims.(*TokenPayload)
+			if !ok {
+				return nil, fmt.Errorf("token expired: %v", err)
+			}
+		}
+
+		return nil, fmt.Errorf("error parsing token: %v", err)
+	}
+
+	payload, ok := parsedToken.Claims.(*TokenPayload)
+	if !ok {
+		return nil, fmt.Errorf("invalid token")
+	}
+
+	return payload, nil
+}
+
+func ParseToken(token string) (*TokenPayload, error) {
+	maker := NewJWTMaker("iDidNotHitHer")
+
+	payload, err := maker.ParseToken(token)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing token: %v", err)
+	}
+
+	return payload, nil
+}
+
+func (j *JWTMaker) ParseExpiredToken(token string) (*TokenPayload, error) {
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, fmt.Errorf("wrong signing method")
+		}
+
+		return []byte(j.secretKey), nil
+	}
+
+	parsedToken, _ := jwt.ParseWithClaims(token, &TokenPayload{}, keyFunc)
+
+	payload, ok := parsedToken.Claims.(*TokenPayload)
+	if !ok {
+		return nil, fmt.Errorf("invalid token")
+	}
+
+	return payload, nil
+
 }
--- internal/core/tokens/tokens.go
+++ internal/core/tokens/tokens.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/NotYourAverageFuckingMisery/animello/internal/models"
+	"github.com/NotYourAverageFuckingMisery/animello/internal/store"
 	"github.com/google/uuid"
 )
 
@@ -26,4 +27,20 @@ func GenerateTokenPair(userID uuid.UUID) (*models.Tokens, uuid.UUID, error) {
 	}
 
 	return &models.Tokens{Acsess: aToken, Refresh: rToken}, rTokenID, nil
+}
+
+func Refresh(rToken string, s *store.Store) (*models.Tokens, error) {
+	maker := NewJWTMaker("iDidNotHitHer")
+	payload, err := maker.ParseExpiredToken(rToken)
+	if err != nil {
+		s.Logger.Error("Failed to parse expired token lol")
+		return nil, err
+	}
+
+	aToken, _, err := maker.CreateToken(payload.UserID, payload.RtokenID, TLifetime)
+	if err != nil {
+		return nil, err
+	}
+
+	return &models.Tokens{Acsess: aToken}, nil
 }
